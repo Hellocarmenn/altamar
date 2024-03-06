b7de809694c7d17b0f3e6c730bc7d35638d85859
carmen <carmenmtz97@comunidad.unam.mx>
2024-03-06 13:46:42 -0600

    primer commit

---
+++ pages/script/alert1.js
@@ -0,0 +1,12 @@
+import Swal from 'sweetalert2';
+const showAlert = () => {
+    Swal.fire({
+        title: "هل تريد الاستمرار؟",
+        icon: "question",
+        iconHtml: "؟",
+        confirmButtonText: "نعم",
+        cancelButtonText: "لا",
+        showCancelButton: true,
+        showCloseButton: true
+    });
+}
